e4f86aa0fa2ac64e02ea0de3c50a55cfa7dede5d
Hiroyuki Hoshi <hoshih@qunie.com>
2022-12-09 03:12:31 +0000

    edit server.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const cds = require('@sap/cds')
 
-//if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production') {
 const cds_swagger = require('cds-swagger-ui-express')
 cds.on('bootstrap', app => app.use(cds_swagger({
     "basePath": "/$api-docs", // the root path to mount the middleware on
     "apiPath": "", // the root path for the services (useful if behind a reverse proxy)
     "diagram": "true" // whether to render the YUML diagram
 })))
-//}
-
+}
 module.exports = cds.server
